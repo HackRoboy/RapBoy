21de16f22d81bab7a019be7f6403af83ef1f59eb
rdcld <mfedosseyeva@gmail.com>
2019-05-04 19:12:27 +0200

    adding leds

--- test_client.py
+++ test_client.py
@@ -12,11 +12,15 @@ def print_stream(stream):
 if __name__ == "__main__":
 
     cli = RoboyClient(server, username, password)
-    cli.send_file("final.wav", "rapboy/final.wav")
-    (stdin, stdout, stderr) = cli.run_command("ls")
+    #cli.remove_remote("rapboy/final.wav")
+    #cli.send_file("final.wav", "rapboy/final.wav")
+    (stdin, stdout, stderr) = cli.run_command("ls rapboy")
+    (stdin, stdout, stderr) = cli.run_command("""docker exec hungry_fermi bash -c 'source ~/melodic_ws/devel/setup.bash'; 
+        ROS_IP=192.168.0.105 rostopic pub /roboy/control/matrix/leds/mode/simple std_msgs/Int32 "data: 1"'""")
+
     #(stdin, stdout, stderr) = cli.run_command("mkdir test")
     #print_stream(stdin)
-    #(stdin, stdout, stderr) = cli.run_command("mpv ./rapboy/final.wav")
+    (stdin, stdout, stderr) = cli.run_command("mpv ./rapboy/final.wav")
     print_stream(stdout)
     print_stream(stderr)
     cli.close()
